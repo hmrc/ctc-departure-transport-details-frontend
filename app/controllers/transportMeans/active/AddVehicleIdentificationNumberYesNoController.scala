6b4abc631ac02e78ae003becec82cb214d600d22
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2023-07-12 14:53:59 +0100

    CTCP-3219: create page for Ass vehicle Identification number

---
+++ app/controllers/transportMeans/active/AddVehicleIdentificationNumberYesNoController.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.transportMeans.active
+
+import controllers.actions._
+import controllers.{NavigatorOps, SettableOps, SettableOpsRunner}
+import forms.YesNoFormProvider
+import models.{Index, LocalReferenceNumber, Mode}
+import navigation.{TransportMeansActiveNavigatorProvider, UserAnswersNavigator}
+import pages.transportMeans.active.AddVehicleIdentificationNumberYesNoPage
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.transportMeans.active.AddVehicleIdentificationNumberYesNoView
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class AddVehicleIdentificationNumberYesNoController @Inject() (implicit
+  val sessionRepository: SessionRepository,
+  navigatorProvider: TransportMeansActiveNavigatorProvider,
+  actions: Actions,
+  formProvider: YesNoFormProvider,
+  val controllerComponents: MessagesControllerComponents,
+  view: AddVehicleIdentificationNumberYesNoView
+) extends FrontendBaseController
+    with I18nSupport {
+
+  private val form = formProvider("transportMeans.active.addVehicleIdentification NumberYesNo")
+
+  def onPageLoad(lrn: LocalReferenceNumber, mode: Mode, activeIndex: Index): Action[AnyContent] = actions.requireData(lrn) {
+    implicit request =>
+      val preparedForm = request.userAnswers.get(AddVehicleIdentificationNumberYesNoPage(activeIndex)) match {
+        case None        => form
+        case Some(value) => form.fill(value)
+      }
+
+      Ok(view(preparedForm, lrn, mode, activeIndex))
+  }
+
+  def onSubmit(lrn: LocalReferenceNumber, mode: Mode, activeIndex: Index): Action[AnyContent] = actions.requireData(lrn).async {
+    implicit request =>
+      form
+        .bindFromRequest()
+        .fold(
+          formWithErrors => Future.successful(BadRequest(view(formWithErrors, lrn, mode, activeIndex))),
+          value => {
+            implicit val navigator: UserAnswersNavigator = navigatorProvider(mode, activeIndex)
+            AddVehicleIdentificationNumberYesNoPage(activeIndex).writeToUserAnswers(value).updateTask().writeToSession().navigate()
+          }
+        )
+  }
+}
