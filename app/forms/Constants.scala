2191d3c77d09e4f8ff111842ab685219de94fc27
BledarFeratiEY <104501743+BledarFeratiEY@users.noreply.github.com>
2023-07-12 16:55:17 +0100

    CTCP-3220: Add Dynamic heading + data format

--- app/forms/Constants.scala
+++ app/forms/Constants.scala
@@ -29,6 +29,6 @@ object Constants {
   lazy val itemNumberLength: Int                = 4
   lazy val itemNumberMax: Int                   = 1999
   lazy val maxContainerIdNumberLength: Int      = 17
-  lazy val identificationNumberLength: Int      = 35
+  lazy val identificationNumberLength: Int      = 27
   lazy val meansIdentificationNumberLength: Int = 27
 }
