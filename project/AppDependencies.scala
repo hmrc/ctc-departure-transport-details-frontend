fdebd407de15c01bf87f177f3cfadd2786594b09
Reece-Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2023-11-28 11:48:00 +0000

    Fix incorrect plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "7.29.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "8.0.0",
     "uk.gov.hmrc"        %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"     % bootstrapVersion,
     "org.typelevel"      %% "cats-core"                      % "2.9.0",
