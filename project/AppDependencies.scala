83823e63be6c976f1ab1629754dea38d29e78c76
Reece-Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2023-11-28 12:33:04 +0000

    Use versions on catalogue

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,9 +7,9 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "8.0.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.29.0-play-28",
     "uk.gov.hmrc"        %% "play-conditional-form-mapping"  % "1.13.0-play-28",
-    "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"     % bootstrapVersion,
+    "uk.gov.hmrc"        %% "bootstrap-frontend-play-28"     % bootstrapVersion,
     "org.typelevel"      %% "cats-core"                      % "2.9.0",
     "com.chuusai"        %% "shapeless"                      % "2.3.10",
     "uk.gov.hmrc"        %% "play-allowlist-filter"          % "1.2.0",
@@ -18,7 +18,7 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "org.scalatest"           %% "scalatest"               % "3.2.17",
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"  % bootstrapVersion,
     "com.typesafe.play"       %% "play-test"               % PlayVersion.current,
     "org.mockito"             %  "mockito-core"            % "5.2.0",
     "org.scalatestplus"       %% "mockito-4-11"            % "3.2.17.0",
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,8 +3,8 @@ resolvers += Resolver.url("HMRC-open-artefacts-ivy2", url("https://open.artefact
 resolvers += Resolver.typesafeRepo("releases")
 
 addSbtPlugin("uk.gov.hmrc"         % "sbt-auto-build"           % "3.15.0")
-addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.4.0")
-addSbtPlugin("org.playframework" % "sbt-plugin"    % "3.0.0")
+addSbtPlugin("uk.gov.hmrc"         % "sbt-distributables"       % "2.4.0")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.9.0")
 addSbtPlugin("org.scoverage"       % "sbt-scoverage"            % "1.9.3")
 addSbtPlugin("net.ground5hark.sbt" % "sbt-concat"               % "0.2.0")
 addSbtPlugin("com.typesafe.sbt"    % "sbt-uglify"               % "2.0.0")
