f0d5ad7bd9b8c7aac4d422ff54e5ca4aa676f8a3
geethumathew <geethuelizebeth@gmail.com>
2026-08-14 10:04:42 +0100

    DL-20032:Dependency upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.6.0"
+  private val bootstrapVersion = "10.7.1"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
