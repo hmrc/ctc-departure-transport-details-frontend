5f52e1d72511019ca1e9d1096cba4ed6241616b4
Geethu Elizebeth Mathew <32937184+geethumathew@users.noreply.github.com>
2026-08-14 11:10:00 +0100

    DL-20032:Implement new service navigation component in ctc-departure-… (#252)
    * DL-20032:Implement new service navigation component in ctc-departure-transport-details-frontend
    
    * DL-20032:Dependency upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.6.0"
+  private val bootstrapVersion = "10.7.1"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "12.31.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "12.32.1",
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "org.typelevel"      %% "cats-core"                              % "2.13.0",
     "org.apache.commons" %  "commons-text"                           % "1.15.0"
